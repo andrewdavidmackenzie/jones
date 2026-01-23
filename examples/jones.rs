88765f125e7019f53640fcf57c246ca2c6fb44ab
Andrew Mackenzie <andrew@mackenzie-serres.net>
2026-01-23 16:33:40 +0100

    Initial commit with a small example that might panic

---
+++ examples/jones.rs
@@ -0,0 +1,9 @@
+use rand::Rng;
+
+const CATCHPHRASES: [&str;3] = ["Don't Panic!", "Permission to speak, sir?", "They don't like it up 'em!"];
+fn main() {
+    // this random number could be out of range of array indexes
+    let num = rand::rng().random_range(0..4);
+    let phrase = CATCHPHRASES[num];
+    println!("{}", phrase);
+}
